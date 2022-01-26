f3aacd8460533f9bbeaff4aa83cc297b1a0d46a4
yasjpn <97491877+yasjpn@users.noreply.github.com>
2022-01-26 20:10:59 +0900

    user listing

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,42 @@
-import { memo, FC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  Box,
+  Center,
+  Image,
+  Spinner,
+  Stack,
+  Text,
+  Wrap,
+  WrapItem
+} from "@chakra-ui/react";
+import { memo, FC, useEffect } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
-  return <p>User Managemenrt Page</p>;
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
