10a42d06045789a509caae4900545a0696bc3385
yasjpn <97491877+yasjpn@users.noreply.github.com>
2022-03-27 14:27:22 +0900

    commit

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,10 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { User } from "../types/api/user";
 
 import { useMessage } from "./useMessage";
+import { useLoginUser } from "./useLoginUser";
 
 export const useAuth = () => {
   const navigate = useNavigate();
   const { showMessage } = useMessage();
+  const { setLoginUser } = useLoginUser();
 
   const [loading, setLoading] = useState(false);
 
@@ -19,6 +21,7 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            setLoginUser(res.data);
             showMessage({ title: "Login success", status: "success" });
             navigate("/home");
           } else {
@@ -28,7 +31,7 @@ export const useAuth = () => {
         .catch(() => showMessage({ title: "can not login", status: "error" }))
         .finally(() => setLoading(false));
     },
-    [navigate, showMessage]
+    [navigate, showMessage, setLoginUser]
   );
   return { login, loading };
 };
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,20 +5,23 @@ import { Login } from "../components/pages/Login";
 import { homeRoute } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
 import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { LoginUserProvider } from "../providers/LoginUserProvider";
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="home">
-        {homeRoute.map((route, i) => (
-          <Route
-            key={i}
-            path={route.path}
-            element={<HeaderLayout>{route.children}</HeaderLayout>}
-          />
-        ))}
-      </Route>
+      <LoginUserProvider>
+        <Route path="/" element={<Login />} />
+        <Route path="home">
+          {homeRoute.map((route, i) => (
+            <Route
+              key={i}
+              path={route.path}
+              element={<HeaderLayout>{route.children}</HeaderLayout>}
+            />
+          ))}
+        </Route>
+      </LoginUserProvider>
       <Route path="*" element={<Page404 />} />
     </Routes>
   );
